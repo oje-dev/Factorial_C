2ee8a03bebffe8373c82059b242a10e40b9058d5
oje-dev <44790306+oje-dev@users.noreply.github.com>
2020-05-11 15:25:37 +0100

    Implemented factorial() function.

--- factorial.c
+++ factorial.c
@@ -1,26 +1,39 @@
-/* 
+/*
  * factorial.c - the implementation of factorial functions.
- * 
+ *
  * This is the file you edit.
- * 
- * Note: all of the factorial functions will cause int overflow for large 
+ *
+ * Note: all of the factorial functions will cause int overflow for large
  * enough n.
  */
 #include <errno.h>
 #include "factorial.h"
 
-/* 
- * See factorial.h for the specification of this iterative factorial 
+/*
+ * See factorial.h for the specification of this iterative factorial
  * function.
  *
  * You have to implement this function.
  */
 int factorial(int n) {
-    return -1;
+    if (n < 0) {
+        errno = EINVAL;
+        return -1;
+
+    }else if (n == 0) {
+        return 1;
+
+    }else{
+        unsigned long fact = 1;
+        for(int i = 1; i <= n; i++){
+            fact = fact * i;
+        }
+        return fact;
+    }
 }
 
-/* 
- * See factorial.h for the specification of this recursive factorial 
+/*
+ * See factorial.h for the specification of this recursive factorial
  * function.
  *
  * THIS FUNCTION IS IMPLEMENTED FOR YOU.
@@ -33,12 +46,12 @@ int rfactorial(int n) {
 
     if (n == 0)
         return 1;
-    
+
     return rfactorial(n - 1) * n;
 }
 
-/* 
- * See factorial.h for the specification of this tail-recursive factorial 
+/*
+ * See factorial.h for the specification of this tail-recursive factorial
  * function.
  *
  * You have to implement this function.
